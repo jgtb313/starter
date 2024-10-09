72bfff972dce480f800cda5fced0f0e5dd9f623c
joaotury <joaotury@bluems.com>
2024-10-09 11:33:16 -0300

    eqweqwe

--- apps/api/plopfile.js
+++ apps/api/plopfile.js
@@ -14,97 +14,97 @@ module.exports = function (plop) {
       },
     ],
     actions: [
-      // {
-      //   type: 'add',
-      //   path: 'src/ports/database/modules/{{pascalCase name}}.repository.ts',
-      //   templateFile: 'templates/module/ports/database/port.database.ts.hbs',
-      // },
-      // {
-      //   type: 'modify',
-      //   path: 'src/ports/database/modules/index.ts',
-      //   pattern: /(\/\/ appendModuleImportHere)/,
-      //   template: "import { I{{ pascalCase name }}Repository } from './{{ pascalCase name }}.repository'\n$1",
-      // },
-      // {
-      //   type: 'modify',
-      //   path: 'src/ports/database/modules/index.ts',
-      //   pattern: /(\/\/ appendModuleTypeHere)/,
-      //   template: '{{ camelCase name }}: ReturnType<I{{ pascalCase name }}Repository>\n$1',
-      // },
+      {
+        type: 'add',
+        path: 'src/ports/database/modules/{{pascalCase name}}.repository.ts',
+        templateFile: 'templates/module/ports/database/port.database.ts.hbs',
+      },
+      {
+        type: 'modify',
+        path: 'src/ports/database/modules/index.ts',
+        pattern: /(\/\/ appendModuleImportHere)/,
+        template: "import { I{{ pascalCase name }}Repository } from './{{ pascalCase name }}.repository'\n$1",
+      },
+      {
+        type: 'modify',
+        path: 'src/ports/database/modules/index.ts',
+        pattern: /(\/\/ appendModuleTypeHere)/,
+        template: '{{ camelCase name }}: ReturnType<I{{ pascalCase name }}Repository>\n$1',
+      },
 
-      // {
-      //   type: 'add',
-      //   path: 'src/ports/http/modules/{{pascalCase name}}.http.ts',
-      //   templateFile: 'templates/module/ports/http/port.http.ts.hbs',
-      // },
-      // {
-      //   type: 'modify',
-      //   path: 'src/ports/http/modules/index.ts',
-      //   pattern: /(\/\/ appendHere)/,
-      //   template: "export * from './{{ pascalCase name }}.http'\n$1",
-      // },
+      {
+        type: 'add',
+        path: 'src/ports/http/modules/{{pascalCase name}}.http.ts',
+        templateFile: 'templates/module/ports/http/port.http.ts.hbs',
+      },
+      {
+        type: 'modify',
+        path: 'src/ports/http/modules/index.ts',
+        pattern: /(\/\/ appendHere)/,
+        template: "export * from './{{ pascalCase name }}.http'\n$1",
+      },
 
-      // {
-      //   type: 'add',
-      //   path: 'src/adapters/mongodb/modules/{{pascalCase name}}.mongodb.ts',
-      //   templateFile: 'templates/module/adapters/mongodb/adapter.mongodb.ts.hbs',
-      // },
-      // {
-      //   type: 'modify',
-      //   path: 'src/adapters/mongodb/modules/index.ts',
-      //   pattern: /(\/\/ appendAdapterImportHere)/,
-      //   template: "import { {{ camelCase name }} } from './{{ pascalCase name }}.mongodb'\n$1",
-      // },
-      // {
-      //   type: 'modify',
-      //   path: 'src/adapters/mongodb/modules/index.ts',
-      //   pattern: /(\/\/ appendAdapterRepositoryHere)/,
-      //   template: '{{ camelCase name }}: {{ camelCase name }}(Collections)(),\n$1',
-      // },
-      // {
-      //   type: 'modify',
-      //   path: 'src/adapters/mongodb/MongoDB.collections.ts',
-      //   pattern: /(\/\/ appendCollectionImportHere)/,
-      //   template: "import { {{ pascalCase name }} } from '@/core/{{ kebabCase name }}/domain'\n$1",
-      // },
-      // {
-      //   type: 'modify',
-      //   path: 'src/adapters/mongodb/MongoDB.collections.ts',
-      //   pattern: /(\/\/ appendCollectionTypeHere)/,
-      //   template: "{{ camelCase name }}: Searchable<{{ pascalCase name }}['state']>\n$1",
-      // },
-      // {
-      //   type: 'modify',
-      //   path: 'src/adapters/mongodb/MongoDB.collections.ts',
-      //   pattern: /(\/\/ appendCollectionHere)/,
-      //   template:
-      //     "Collections.{{ camelCase name }} = await createCollectionMongoDB<ICollections['{{ camelCase name }}']>(database, '{{ camelCase namePlural }}')\n$1",
-      // },
-      // {
-      //   type: 'modify',
-      //   path: 'src/adapters/mongodb-in-memory/MongoDB.in-memory.mock.ts',
-      //   pattern: /(\/\/ appendMockImportHere)/,
-      //   template: "import { {{ camelCase name }}Mocks } from '@/core/{{ kebabCase name }}/{{ pascalCase name }}.mock'\n$1",
-      // },
-      // {
-      //   type: 'modify',
-      //   path: 'src/adapters/mongodb-in-memory/MongoDB.in-memory.mock.ts',
-      //   pattern: /(\/\/ appendMockHere)/,
-      //   template: "{ name: '{{ camelCase namePlural }}', data: {{ camelCase name }}Mocks, },\n$1",
-      // },
+      {
+        type: 'add',
+        path: 'src/adapters/mongodb/modules/{{pascalCase name}}.mongodb.ts',
+        templateFile: 'templates/module/adapters/mongodb/adapter.mongodb.ts.hbs',
+      },
+      {
+        type: 'modify',
+        path: 'src/adapters/mongodb/modules/index.ts',
+        pattern: /(\/\/ appendAdapterImportHere)/,
+        template: "import { {{ camelCase name }} } from './{{ pascalCase name }}.mongodb'\n$1",
+      },
+      {
+        type: 'modify',
+        path: 'src/adapters/mongodb/modules/index.ts',
+        pattern: /(\/\/ appendAdapterRepositoryHere)/,
+        template: '{{ camelCase name }}: {{ camelCase name }}(Collections)(),\n$1',
+      },
+      {
+        type: 'modify',
+        path: 'src/adapters/mongodb/MongoDB.collections.ts',
+        pattern: /(\/\/ appendCollectionImportHere)/,
+        template: "import { {{ pascalCase name }} } from '@/core/{{ kebabCase name }}/domain'\n$1",
+      },
+      {
+        type: 'modify',
+        path: 'src/adapters/mongodb/MongoDB.collections.ts',
+        pattern: /(\/\/ appendCollectionTypeHere)/,
+        template: "{{ camelCase name }}: Searchable<{{ pascalCase name }}['state']>\n$1",
+      },
+      {
+        type: 'modify',
+        path: 'src/adapters/mongodb/MongoDB.collections.ts',
+        pattern: /(\/\/ appendCollectionHere)/,
+        template:
+          "Collections.{{ camelCase name }} = await createCollectionMongoDB<ICollections['{{ camelCase name }}']>(database, '{{ camelCase namePlural }}')\n$1",
+      },
+      {
+        type: 'modify',
+        path: 'src/adapters/mongodb-in-memory/MongoDB.in-memory.mock.ts',
+        pattern: /(\/\/ appendMockImportHere)/,
+        template: "import { {{ camelCase name }}Mocks } from '@/core/{{ kebabCase name }}/{{ pascalCase name }}.mock'\n$1",
+      },
+      {
+        type: 'modify',
+        path: 'src/adapters/mongodb-in-memory/MongoDB.in-memory.mock.ts',
+        pattern: /(\/\/ appendMockHere)/,
+        template: "{ name: '{{ camelCase namePlural }}', data: {{ camelCase name }}Mocks, },\n$1",
+      },
 
-      // {
-      //   type: 'addMany',
-      //   destination: 'src/core/{{kebabCase name}}/domain',
-      //   base: 'templates/module/core/domain',
-      //   templateFiles: 'templates/module/core/domain/*.hbs',
-      // },
-      // {
-      //   type: 'addMany',
-      //   destination: 'src/core/{{kebabCase name}}/use-cases',
-      //   base: 'templates/module/core/use-cases',
-      //   templateFiles: 'templates/module/core/use-cases/*.hbs',
-      // },
+      {
+        type: 'addMany',
+        destination: 'src/core/{{kebabCase name}}/domain',
+        base: 'templates/module/core/domain',
+        templateFiles: 'templates/module/core/domain/*.hbs',
+      },
+      {
+        type: 'addMany',
+        destination: 'src/core/{{kebabCase name}}/use-cases',
+        base: 'templates/module/core/use-cases',
+        templateFiles: 'templates/module/core/use-cases/*.hbs',
+      },
       {
         type: 'add',
         path: 'src/core/{{kebabCase name}}/{{pascalCase name}}.mock.ts',
