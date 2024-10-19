671d7e1c8fa52c88c33704a10ddc5c9c1fb71745
joaotury <joaotury@bluems.com>
2024-10-19 11:34:08 -0300

    eqweqe

--- plopfile.js
+++ plopfile.js
@@ -6,38 +6,61 @@ const createCommand = (template, values) =>
 module.exports = function (plop) {
   plop.setActionType('shell', (answers, config) => {
     const command = createCommand(config.command, answers)
+
+    console.log(config.message)
+
     exec(command)
   })
 
   plop.setGenerator('generate', {
     description: 'Generate schema, api client resource, and api module',
-    prompts: [
-      {
-        type: 'input',
-        name: 'name',
-        message: 'Name:',
-      },
-      {
-        type: 'input',
-        name: 'namePlural',
-        message: 'Plural:',
-      },
-    ],
+    prompts: async function (inquirer) {
+      const { name } = await inquirer.prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'Schema name:',
+          validate: (input) => (input ? true : 'Required'),
+        },
+      ])
+
+      const defaultNamePlural = `${name}s`
+
+      const { namePlural } = await inquirer.prompt([
+        {
+          type: 'input',
+          name: 'namePlural',
+          message: 'Plural:',
+          default: defaultNamePlural,
+          validate: (input) => (input ? true : 'Required'),
+        },
+      ])
+
+      const form = { name, namePlural }
+
+      return {
+        ...form,
+      }
+    },
     actions: [
       {
         type: 'shell',
+        message: 'Generating Schema...',
         command: 'yarn workspace @starter/schema generate:schema "{{name}}" "{{namePlural}}"',
       },
       {
         type: 'shell',
+        message: 'Generating API Client Resource...',
         command: 'yarn workspace @starter/client generate:resource "{{name}}" "{{namePlural}}"',
       },
       {
         type: 'shell',
+        message: 'Generating API Module...',
         command: 'yarn workspace @starter/api generate:module "{{name}}" "{{namePlural}}"',
       },
       {
         type: 'shell',
+        message: 'Building...',
         command: 'yarn workspace @starter/shared build && yarn workspace @starter/schema build && yarn workspace @starter/client build',
       },
     ],
