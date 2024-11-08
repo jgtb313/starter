d3e5520405cddc8bda6d8bbef42b287f9bcbf2f5
joaotury <joaotury@bluems.com>
2024-11-07 22:17:27 -0300

    eqweqwe

--- apps/console-web/app/components/ToggleColorScheme/ToggleColorScheme.types.ts
+++ apps/console-web/app/components/ToggleColorScheme/ToggleColorScheme.types.ts
@@ -1,4 +1,4 @@
-import { ButtonProps } from '@ss/components'
+import { ButtonProps } from '@starter/ui'
 
 export type ToggleColorSchemeProps = {
   size?: ButtonProps['size']
--- artifacts/ui/src/components/Icon/Icon.tsx
+++ artifacts/ui/src/components/Icon/Icon.tsx
@@ -1,12 +1,14 @@
 import { forwardRef, Ref } from 'react'
-import { icons, Icon as Component } from '@tabler/icons-react'
+import { IconHome, Icon as Component } from '@tabler/icons-react'
 
 import { IconProps } from './Icon.types'
 
 const BaseIcon = ({ name, ...props }: IconProps, ref: Ref<Component>) => {
-  const Icon = icons[name]
+  // const Icon = icons[name]
 
-  return <Icon ref={ref} {...props} />
+  // return <Icon ref={ref} {...props} />
+
+  return <IconHome ref={ref} {...props} />
 }
 
 export const Icon = forwardRef(BaseIcon)
