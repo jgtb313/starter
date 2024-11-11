a6e25ab24f8808291394084c5bd3d2ee3a5a757a
joaotury <joaotury@bluems.com>
2024-11-11 10:46:57 -0300

    eqwe

--- apps/api/src/config/tests/dependencies.ts
+++ apps/api/src/config/tests/dependencies.ts
@@ -2,13 +2,15 @@ import { vi, Mock } from 'vitest'
 
 import { IDependencies } from '@/support/types'
 import { DatabaseInMemory } from '@/adapters/mongodb-in-memory'
-import { JWTInMemory } from '@/adapters/json-web-token-in-memory'
 import { EncryptInMemory } from '@/adapters/bcrypt-in-memory'
-import { SocialAuthInMemory } from '@/adapters/social-auth-in-memory'
-import { StorageInMemory } from '@/adapters/aws-s3-in-memory'
+import { JWTInMemory } from '@/adapters/json-web-token-in-memory'
 import { MailInMemory } from '@/adapters/google-mail-in-memory'
 import { TwilioSMSInMemory } from '@/adapters/twilio-sms-in-memory'
 import { TwilioWhatsappInMemory } from '@/adapters/twilio-whatsapp-in-memory'
+import { CacheInMemory } from '@/adapters/redis-in-memory'
+import { StorageInMemory } from '@/adapters/aws-s3-in-memory'
+import { SocialAuthInMemory } from '@/adapters/social-auth-in-memory'
+import { LoggerInMemory } from '@/adapters/pino-es-in-memory'
 
 export type SetupTestDependencies<T> = {
   [K in keyof T]: T[K] extends (...args: infer A) => infer R ? Mock<(...args: A) => R> : T[K] extends object ? SetupTestDependencies<T[K]> : T[K]
@@ -19,8 +21,6 @@ export type ITestDependencies = SetupTestDependencies<IDependencies>
 export const TestDependencies = async (): Promise<ITestDependencies> => {
   vi.clearAllMocks()
 
-  vi.stubEnv('PROJECT', 'test')
-  vi.stubEnv('PROJECT_DOMAIN', 'test.com')
   vi.stubEnv('STAGE', 'local')
 
   vi.stubEnv('SERVER_PORT', '4000')
@@ -45,24 +45,28 @@ export const TestDependencies = async (): Promise<ITestDependencies> => {
   vi.stubEnv('TWILIO_WHATSAPP_AUTH_TOKEN', 'fake-whatsapp-auth-token')
   vi.stubEnv('TWILIO_WHATSAPP_FROM', 'whatsapp:+1234567890')
 
+  vi.stubEnv('REDIS_URL', 'redis://localhost:6379')
+  vi.stubEnv('REDIS_PASSWORD', 'fake-redis-password')
+  vi.stubEnv('REDIS_DISABLED', 'true')
+
+  vi.stubEnv('LOGGER_URL', 'http://logger.test.com')
+  vi.stubEnv('LOGGER_USER', 'fake-logger-user')
+  vi.stubEnv('LOGGER_PASSWORD', 'fake-logger-password')
+  vi.stubEnv('LOGGER_DISABLED', 'false')
+
   return {
-    JWT: JWTInMemory,
-
-    Encrypt: EncryptInMemory,
-
-    Mail: MailInMemory,
-
-    SMS: TwilioSMSInMemory,
-
-    Whatsapp: TwilioWhatsappInMemory,
-
-    SocialAuth: SocialAuthInMemory,
-
-    Storage: StorageInMemory,
-
     Database: {
       createSession: DatabaseInMemory.createSession as Mock,
       ...DatabaseInMemory.Repositories,
     },
+    Encrypt: EncryptInMemory,
+    JWT: JWTInMemory,
+    Mail: MailInMemory,
+    SMS: TwilioSMSInMemory,
+    Whatsapp: TwilioWhatsappInMemory,
+    Cache: CacheInMemory,
+    Storage: StorageInMemory,
+    SocialAuth: SocialAuthInMemory,
+    Logger: LoggerInMemory,
   }
 }
