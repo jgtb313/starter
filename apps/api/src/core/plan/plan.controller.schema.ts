aeb282f958e3a4d35cd3af46078f5cfe87f04dd8
João Tury <jgtb313@gmail.com>
2025-04-16 00:55:28 -0300

    eqweqwe

--- apps/api/src/core/plan/plan.controller.schema.ts
+++ apps/api/src/core/plan/plan.controller.schema.ts
@@ -3,7 +3,7 @@ import { PlanSchema } from '@starter/domain'
 import { z, PaginationSchema, BasePaginationSchemaOutput } from '@starter/schema'
 
 export const ListPlansSchema = createRequestSchema({
-  query: PlanSchema.partial().merge(PaginationSchema),
+  query: PlanSchema.pick({}).partial().merge(PaginationSchema),
   output: BasePaginationSchemaOutput.merge(z.object({ values: z.array(PlanSchema) })),
 })
 export type ListPlansRequest = RequestInput<typeof ListPlansSchema>
