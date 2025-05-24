5b736fe2c61edff19c7110de1fc285fd563bc7fe
João Tury <jgtb313@gmail.com>
2025-05-23 21:47:46 -0300

    eqweqwe

--- libs/domain/src/core/invoice/index.ts
+++ libs/domain/src/core/invoice/index.ts
@@ -1,4 +1,3 @@
 export * from './invoice.schema'
-export * from './invoice.service.interface'
 export * from './invoice.service.module'
 export * from './invoice.service'
--- libs/domain/src/core/organization/index.ts
+++ libs/domain/src/core/organization/index.ts
@@ -1,4 +1,3 @@
 export * from './organization.schema'
-export * from './organization.service.interface'
 export * from './organization.service.module'
 export * from './organization.service'
