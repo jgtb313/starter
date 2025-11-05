d0ec422faee6f318333d8ad7b30735878f5e59ea
João Tury <jgtb313@gmail.com>
2025-11-05 01:09:53 -0300

    eqwe

--- apps/console/vite.config.ts
+++ apps/console/vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig, type PluginOption } from 'vite'
 
 export default defineConfig({
+	base: '/console/',
 	plugins: [
 		tanstackRouter({
 			autoCodeSplitting: true,
